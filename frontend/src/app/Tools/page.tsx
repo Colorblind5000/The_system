6d50b3dd234621b30099904ade6d15914563fc61
Colorblind5000 <Emil@pixed.com>
2025-04-18 17:14:05 +0200

    small

--- frontend/src/app/Tools/page.tsx
+++ frontend/src/app/Tools/page.tsx
@@ -3,7 +3,11 @@
 export default function Tools() {
   return (
     <div style={{ padding: '20px' }}>
+<<<<<<< Updated upstream
       <h1>din mor</h1>
+=======
+      <h1>Din mor</h1>
+>>>>>>> Stashed changes
       <p>Browse tools.</p>
     </div>
   );
