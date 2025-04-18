3fb427b9094868b0497dea3be477004d13ef2dbd
Colorblind5000 <Emil@pixed.com>
2025-04-18 17:35:38 +0200

    Update page.tsx

--- frontend/src/app/Tools/page.tsx
+++ frontend/src/app/Tools/page.tsx
@@ -1,14 +1,8 @@
-"use client";
-
 export default function Tools() {
   return (
     <div style={{ padding: '20px' }}>
-<<<<<<< Updated upstream
-      <h1>din mor</h1>
-=======
       <h1>Din mor</h1>
->>>>>>> Stashed changes
-      <p>Browse tools.</p>
+      <p>Browse and run your tools here.</p>
     </div>
   );
 }
